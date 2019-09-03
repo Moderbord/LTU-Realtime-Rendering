9e1f30265363107bf6e43b5b03b4b1135d37475e
Pontus Andersson <ponane-6@student.ltu.se>
2019-09-03 13:14:15 +0200

    Implemented basic vector operations

--- Math Library/Source.cpp
+++ Math Library/Source.cpp
@@ -6,10 +6,16 @@
 
 int main()
 {
-	modmath::Vector<float, 3> vector(3.0f, 4.0f, 3.0f);
-	modmath::Vector<float, 3> vectora(vector);
+	modmath::Vector<float, 4> v1(3.0f, 4.0f, 3.0f, 2.0f);
+	modmath::Vector<float, 4> v2(1.0f, 2.0f, 6.0f, 5.7f);
+
+	modmath::vec4 v3 = v2 + 2.0f;
 
 	modmath::vec2 vec(4.0f, 5.1f);
+
+	modmath::vec2i v1i(4, 5);
+	modmath::vec2i v2i(v1i + 7);
+
 
 	int i = 0;
 }
--- Math Library/vector.h
+++ Math Library/vector.h
@@ -1,6 +1,16 @@
 #pragma once
 #include <yvals.h>
 
+#define MODMATH_VECTOR_OPERATOR(OP, Mod)					\
+	{														\
+		Vector<T, Dims> result;								\
+		for (int i = 0; i < Dims; i++)						\
+		{													\
+			result.data_[i] = this->data_[i] OP Mod;		\
+		}													\
+		return result;										\
+	}
+	
 namespace modmath {
 
 	template <class T, int Dims>
@@ -16,33 +26,86 @@ namespace modmath {
 		inline Vector(T v1, T v2)
 		{
 			_STL_VERIFY(Dims == 2, "Size of vectors does not match");
-			data_[0] = v1;
-			data_[1] = v2;
+			this->data_[0] = v1;
+			this->data_[1] = v2;
 		}
 		// Create a vector from 3 values
 		inline Vector(T v1, T v2, T v3)
 		{
 			_STL_VERIFY(Dims == 3, "Size of vectors does not match");
-			data_[0] = v1;
-			data_[1] = v2;
-			data_[2] = v3;
+			this->data_[0] = v1;
+			this->data_[1] = v2;
+			this->data_[2] = v3;
 		}
 		// Create a vector from 4 values
 		inline Vector(T v1, T v2, T v3, T v4)
 		{
 			_STL_VERIFY(Dims == 4, "Size of vectors does not match");
-			data_[0] = v1;
-			data_[1] = v2;
-			data_[2] = v3;
-			data_[3] = v4;
+			this->data_[0] = v1;
+			this->data_[1] = v2;
+			this->data_[2] = v3;
+			this->data_[3] = v4;
 		}
+
 		// Create a vector from another vector
 		inline Vector(const Vector<T, Dims> &v)
 		{
-			for (int i = Dims - 1; i > -1; i--)
+			for (int i = 0; i < Dims; i++)
 			{
-				data_[i] = v.data_[i];
+				this->data_[i] = v.data_[i];
 			}
+		}
+
+		// Addition between two vectors
+		template <class T, int Dims>
+		inline Vector<T, Dims> operator+(const Vector<T, Dims> &v)
+		{
+			MODMATH_VECTOR_OPERATOR(+, v.data_[i]);
+		}
+
+		// Subtraction between two vectors
+		template <class T, int Dims>
+		inline Vector<T, Dims> operator-(const Vector<T, Dims>& v)
+		{
+			MODMATH_VECTOR_OPERATOR(-, v.data_[i]);
+		}
+
+		// Multiplication between two vectors
+		template <class T, int Dims>
+		inline Vector<T, Dims> operator*(const Vector<T, Dims>& v)
+		{
+			MODMATH_VECTOR_OPERATOR(*, v.data_[i]);
+		}
+
+		// Division between two vectors
+		template <class T, int Dims>
+		inline Vector<T, Dims> operator/(const Vector<T, Dims>& v)
+		{
+			MODMATH_VECTOR_OPERATOR(/ , v.data_[i]);
+		}
+
+		// Addition between a vector and a scalar
+		Vector<T, Dims> operator+(const T s)
+		{
+			MODMATH_VECTOR_OPERATOR(+, s);
+		}
+
+		// Subtraction between a vector and a scalar
+		Vector<T, Dims> operator-(const T s)
+		{
+			MODMATH_VECTOR_OPERATOR(-, s);
+		}
+
+		// Multiplication between a vector and a scalar
+		Vector<T, Dims> operator*(const T s)
+		{
+			MODMATH_VECTOR_OPERATOR(*, s);
+		}
+
+		// Division between a vector and a scalar
+		Vector<T, Dims> operator/(const T s)
+		{
+			MODMATH_VECTOR_OPERATOR(/, s);
 		}
 
 	};
